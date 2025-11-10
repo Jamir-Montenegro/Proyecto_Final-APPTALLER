0c5e7d6d38b1b92f3018a1abea99c0eb4f1f316b
Jamir.M <71949424+Jamir-Montenegro@users.noreply.github.com>
2025-11-10 17:49:56 -0500

    Actualización dentro del submódulo

---
+++ Frontend/scripts/002_seed_data.sql
@@ -0,0 +1,5 @@
+-- This script will seed some sample data after a user is authenticated
+-- Note: Replace 'YOUR_USER_ID' with actual user_id after authentication
+
+-- Sample data will be added through the UI after authentication
+-- This file is a placeholder for future seeding needs
